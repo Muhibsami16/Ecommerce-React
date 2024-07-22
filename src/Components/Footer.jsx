1c83c233f0f63c9673d35382a0f2aab722ad399d
Muhib Sami <153531960+Muhibsami16@users.noreply.github.com>
2024-07-22 17:47:12 +0500

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-3 bg-secondary d-flex justify-content-center'>
+        <p className=' fs-3  fw-bolder text-white'> Muhib Sami</p>
+        <p className=' fs-4  fw-bolder text-white'>&copy; Copyright 2024.All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
